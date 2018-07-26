61706d12e1cdad68ea13db32c0fa1b6a67e572ac
Alex Anderson <alexanderson1993@gmail.com>
2018-07-26 16:20:05 -0600

    Improve search to search for similar names.

--- src/threeView/update/search.js
+++ src/threeView/update/search.js
@@ -1,11 +1,24 @@
+import leven from "leven";
+import _ from "lodash/fp";
+
+const similarityScore = (strA, strB) => {
+  const aWords = strA.trim().split(" ");
+  const bWords = strB.trim().split(" ");
+
+  return _.flow(
+    _.map(a => _.map(b => leven(a, b), bWords)),
+    _.map(_.min),
+    _.sum
+  )(aWords);
+};
+
 export default function searchUpdate(self, stars) {
   // Search for stars
   const { search } = self.props;
-  const regex = new RegExp(search, "gi");
   const visibleStars = stars.filter(c => {
-    if (c.name.match(regex) || !search) {
-      return true;
-    }
+    if (!search) return true;
+    if (c.name.toLowerCase().indexOf(search.toLowerCase()) !== -1) return true;
+    if (similarityScore(search, c.name) <= 2) return true;
     return false;
   });
   stars.forEach(s => {
